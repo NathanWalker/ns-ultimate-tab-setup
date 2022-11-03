4818742bcbd48571c52a2e193fcce730978e1022
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2022-11-03 13:50:43 +0800

    feat: Add font-awesome

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const webpack = require("@nativescript/webpack");
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env) => {
 	webpack.init(env);
 
-	// Learn how to customize:
-	// https://docs.nativescript.org/webpack
+  webpack.chainWebpack((config) => {
+    config.plugin('font-awesome').use(CopyWebpackPlugin, [
+      {
+        patterns: [
+          {
+            from: resolve(
+              __dirname,
+              'node_modules/font-awesome/fonts/fontawesome-webfont.ttf'
+            ),
+            to: 'fonts',
+          },
+        ],
+      },
+    ]);
+  });
 
 	return webpack.resolveConfig();
 };
