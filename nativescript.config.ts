f91232ec93f4e0ff6bc36d670d887ab618214c10
Nathan Walker <walkerrunpdx@gmail.com>
2025-11-06 22:41:28 -0800

    chore: bundle id

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.tabsample',
+  id: 'io.nstudio.nsngtabsample',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
